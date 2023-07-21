7ff990be5541789ceea8580a72bceafc2a103857
watanabe7 <wooin123480@gmail.com>
2023-07-21 18:57:21 +0900

    QueueLinkedListVersion ( comp test & debug )

---
+++ CHAPTER 7 Queue/QueueLinkedListVersion/TestClient.c
@@ -0,0 +1,43 @@
+#include "Queue.h"
+
+int main() {
+
+	Queue k = InitQueue(); 
+
+	
+
+	EnQueue(k, 1);
+	printf("%d\n", DeQueue(k)); 
+
+	EnQueue(k, 2);
+	EnQueue(k, 3);
+	EnQueue(k, 4);
+	printf("%d\n", DeQueue(k));
+	
+	EnQueue(k, 5);
+
+	printf("%d\n", DeQueue(k));
+	printf("%d\n", DeQueue(k));
+	printf("%d\n", GetFrontItem(k));
+	printf("%d\n", DeQueue(k));
+
+	EnQueue(k, 100);
+	printf("%d", DeQueue(k));
+
+	RemoveQueue(&k);
+
+	EnQueue(k, 1);
+
+
+
+
+	
+
+
+
+
+
+
+
+	return 0;
+}
